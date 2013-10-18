e119a2c1bed9df293296002d3de0f15e2911d3f8
Neil Middleton <neil@heroku.com>
2013-10-18 17:23:35 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'twitter-bootstrap-rails'
+gem "therubyracer"
+gem "less-rails"
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DojoDemo::Application.config.secret_key_base = '4e9503d33641744eab0172f885051e2efadaa713e607629c4db799207cfffcfa7b8c3f1812b20551fe78b1eafc5e6ae530265c55200a6e835f0f34ee086927f5'
